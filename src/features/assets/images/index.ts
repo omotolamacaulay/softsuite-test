f38b4d23806bdd3b25f6fc6d2374d2391eae0269
Omotola Macaulay <tolamacaulay@gmail.com>
2023-09-23 23:26:59 +0100

    Download sidebar and navbar icons

---
+++ src/features/assets/images/index.ts
@@ -0,0 +1,39 @@
+import Logo from "./logo.svg"
+import ArrowDown from "./arrow-down.svg"
+import PayrollMgt from "./payroll-mgt.svg"
+import Dashboard from "./dashboard.svg"
+import PayrollAct from "./payroll-activities.svg"
+import SalaryStructure from "./salary-structure.svg"
+import Settings from "./settings.svg"
+import ArrowUp from "./arrow-up.svg"
+import Profile from "./profile.svg"
+import Logout from "./logout.svg"
+import Home from "./home.svg"
+import Search from "./search.svg"
+import Notification from "./notification.png"
+import User from "./user.png"
+import Close from "./icon-close.svg"
+import Hamburger from "./icon-hamburger.svg"
+import Employees from "./employees.svg"
+
+const Icons: { [key: string]: string } = {
+  Logo,
+  ArrowDown,
+  ArrowUp,
+  PayrollMgt,
+  Dashboard,
+  PayrollAct,
+  SalaryStructure,
+  Settings,
+  Profile,
+  Logout,
+  Home,
+  Search,
+  Notification,
+  User,
+  Close,
+  Hamburger,
+  Employees,
+}
+
+export default Icons
