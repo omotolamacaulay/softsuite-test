60a77779a0070e208665dfa96d9ed967070adc5e
Omotola Macaulay <tolamacaulay@gmail.com>
2023-09-23 23:30:42 +0100

    Create navbar link structure

---
+++ src/features/lib/data.ts
@@ -0,0 +1,106 @@
+import { NavItem } from "../../types"
+
+type navLinksType = NavItem[]
+
+const navlinks: navLinksType = [
+  {
+    title: "Payroll Activities",
+    icon: "PayrollAct",
+    path: "/#",
+    iconClosed: "ArrowDown",
+    iconOpen: "ArrowUp",
+    subNav: [
+      {
+        title: "Payroll Run",
+        path: "link",
+      },
+      {
+        title: "Payroll Reversal",
+        path: "link",
+      },
+      {
+        title: "Payroll History",
+        path: "link",
+      },
+      {
+        title: "Payroll Lock",
+        path: "link",
+      },
+      {
+        title: "Payroll Payslip",
+        path: "link",
+      },
+      {
+        title: "Payroll Log",
+        path: "link",
+      },
+      {
+        title: "Payroll Approval",
+        path: "link",
+      },
+    ],
+  },
+  {
+    title: "Salary Structure",
+    icon: "SalaryStructure",
+    path: "/#",
+  },
+  {
+    title: "Element Setup",
+    icon: "Settings",
+    path: "/#",
+    iconClosed: "ArrowDown",
+    iconOpen: "ArrowUp",
+    subNav: [
+      {
+        title: "Elements",
+        path: "link",
+      },
+      {
+        title: "Balances",
+        path: "link",
+      },
+    ],
+  },
+  {
+    title: "Employees",
+    icon: "Employees",
+    path: "/#",
+  },
+  {
+    title: "Payroll Settings",
+    icon: "Settings",
+    path: "/#",
+    iconClosed: "ArrowDown",
+    iconOpen: "ArrowUp",
+    subNav: [
+      {
+        title: "Payroll Options",
+        path: "link",
+      },
+      {
+        title: "Deduction Account Setup",
+        path: "link",
+      },
+      {
+        title: "Variation Portal Period",
+        path: "link",
+      },
+      {
+        title: "Payrgroup Setup",
+        path: "link",
+      },
+      {
+        title: "Tax Setup",
+        path: "link",
+      },
+    ],
+  },
+  {
+    title: "My Account",
+    icon: "Profile",
+    path: "/#",
+  },
+]
+
+export { navlinks }
