61ff56261f5b6b3cbbac82a99a2c365d77069217
Omotola Macaulay <tolamacaulay@gmail.com>
2023-09-28 22:51:11 +0100

    Implement redux store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,9 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
-import counterReducer from "../features/counter/counterSlice"
+import elementsReducer from "../features/counter/elementSlice"
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    elements: elementsReducer,
   },
 })
 
